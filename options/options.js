fbc6aea9c6d6f649e4279b61c6b397ed4d59651c
Crowdbotics <team@crowdbotics.com>
2023-09-29 21:13:47 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "sweet-moon-43669",
+  url: "https://sweet-moon-43669.botics.co",
+  api: "https://sweet-moon-43669.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
